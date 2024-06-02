16f6f137d1103418fcde10074acb1912dc582c6e
Magma73 <marine.magnin73@gmail.com>
2024-06-02 14:23:33 +0200

    Build : modif vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/hrnet/', // Remplacez 'nom-du-repo' par le nom de votre dépôt GitHub
   plugins: [react()],
-  build: {
-    outDir: 'dist'
-  }
 })
