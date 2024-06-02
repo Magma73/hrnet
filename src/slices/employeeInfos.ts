19e83ca6c98f0a785b428ee2b265195c9d09d212
Magma73 <marine.magnin73@gmail.com>
2024-06-02 14:13:58 +0200

    Build : modification configuration pour build vite config

--- src/slices/employeeInfos.ts
+++ src/slices/employeeInfos.ts
@@ -1,28 +1,5 @@
-// import { createSlice } from '@reduxjs/toolkit';
-
-// const initialState = {
-//   employees: [],
-// };
-
-// const employeeInfos = createSlice({
-//   name: 'employee',
-//   initialState,
-//   reducers: {
-//     addEmployeeInfos: (state, action) => {
-//       state.employees.push(action.payload);
-//     },
-//   },
-// });
-
-// const { reducer, actions } = employeeInfos;
-
-// export const { addEmployeeInfos } = actions;
-// export default reducer;
-
-
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
-// Définir le type pour une information sur un employé
 interface EmployeeInfo {
   firstName: string;
   lastName: string;
@@ -35,17 +12,14 @@ interface EmployeeInfo {
   department: string;
 }
 
-// Définir le type pour l'état initial
 interface EmployeeState {
   employees: EmployeeInfo[];
 }
 
-// Initialiser l'état initial
 const initialState: EmployeeState = {
   employees: [],
 };
 
-// Créer le slice
 const employeeSlice = createSlice({
   name: 'employee',
   initialState,
@@ -56,6 +30,5 @@ const employeeSlice = createSlice({
   },
 });
 
-// Exporter les actions et le reducer
 export const { addEmployeeInfos } = employeeSlice.actions;
 export default employeeSlice.reducer;
